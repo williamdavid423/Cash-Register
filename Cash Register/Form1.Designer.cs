29930ade70241693f92a57111a6710f81faccb6d
billy david <williamdavid423@hotmail.com>
2021-02-11 13:00:42 -0500

    finished without new order button

--- Cash Register/Form1.Designer.cs
+++ Cash Register/Form1.Designer.cs
@@ -50,12 +50,19 @@
             this.taxAmountLabel = new System.Windows.Forms.Label();
             this.totalAmountLabel = new System.Windows.Forms.Label();
             this.changeAmountLabel = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.receiptTitleLabel = new System.Windows.Forms.Label();
+            this.itemsLabel = new System.Windows.Forms.Label();
+            this.itemPriceLabel = new System.Windows.Forms.Label();
+            this.receiptTaxLabel = new System.Windows.Forms.Label();
+            this.receiptTaxAmount = new System.Windows.Forms.Label();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.tenderedAmount = new System.Windows.Forms.Label();
+            this.niceDayLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // storeTitle
             // 
-            this.storeTitle.BackColor = System.Drawing.Color.DodgerBlue;
+            this.storeTitle.BackColor = System.Drawing.Color.LightBlue;
             this.storeTitle.Font = new System.Drawing.Font("Amelia BT", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.storeTitle.Location = new System.Drawing.Point(-2, -2);
             this.storeTitle.Name = "storeTitle";
@@ -98,6 +105,7 @@
             // 
             // textBox1
             // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(236, 93);
             this.textBox1.Name = "textBox1";
@@ -107,14 +115,17 @@
             // 
             // textBox4
             // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox4.Location = new System.Drawing.Point(236, 121);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(56, 22);
             this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.TextBox4_TextChanged);
             // 
             // textBox5
             // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox5.Location = new System.Drawing.Point(236, 155);
             this.textBox5.Name = "textBox5";
@@ -204,6 +215,7 @@
             // 
             // tenderedTextbox
             // 
+            this.tenderedTextbox.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.tenderedTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tenderedTextbox.Location = new System.Drawing.Point(130, 342);
             this.tenderedTextbox.Name = "tenderedTextbox";
@@ -224,7 +236,7 @@
             this.receiptLabel.ForeColor = System.Drawing.SystemColors.ControlText;
             this.receiptLabel.Location = new System.Drawing.Point(342, 93);
             this.receiptLabel.Name = "receiptLabel";
-            this.receiptLabel.Size = new System.Drawing.Size(317, 345);
+            this.receiptLabel.Size = new System.Drawing.Size(317, 320);
             this.receiptLabel.TabIndex = 19;
             // 
             // subLabel
@@ -262,22 +274,101 @@
             this.changeAmountLabel.TabIndex = 23;
             this.changeAmountLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // label2
+            // receiptTitleLabel
             // 
-            this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.label2.Location = new System.Drawing.Point(414, 121);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(169, 23);
-            this.label2.TabIndex = 24;
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.receiptTitleLabel.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.receiptTitleLabel.Location = new System.Drawing.Point(414, 121);
+            this.receiptTitleLabel.Name = "receiptTitleLabel";
+            this.receiptTitleLabel.Size = new System.Drawing.Size(169, 23);
+            this.receiptTitleLabel.TabIndex = 24;
+            this.receiptTitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // itemsLabel
+            // 
+            this.itemsLabel.AutoSize = true;
+            this.itemsLabel.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.itemsLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.itemsLabel.Location = new System.Drawing.Point(377, 173);
+            this.itemsLabel.Name = "itemsLabel";
+            this.itemsLabel.Size = new System.Drawing.Size(0, 13);
+            this.itemsLabel.TabIndex = 25;
+            this.itemsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // itemPriceLabel
+            // 
+            this.itemPriceLabel.AutoSize = true;
+            this.itemPriceLabel.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.itemPriceLabel.Location = new System.Drawing.Point(568, 173);
+            this.itemPriceLabel.Name = "itemPriceLabel";
+            this.itemPriceLabel.Size = new System.Drawing.Size(0, 13);
+            this.itemPriceLabel.TabIndex = 26;
+            this.itemPriceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // receiptTaxLabel
+            // 
+            this.receiptTaxLabel.AutoSize = true;
+            this.receiptTaxLabel.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.receiptTaxLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.receiptTaxLabel.Location = new System.Drawing.Point(377, 248);
+            this.receiptTaxLabel.Name = "receiptTaxLabel";
+            this.receiptTaxLabel.Size = new System.Drawing.Size(0, 13);
+            this.receiptTaxLabel.TabIndex = 27;
+            this.receiptTaxLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // receiptTaxAmount
+            // 
+            this.receiptTaxAmount.AutoSize = true;
+            this.receiptTaxAmount.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.receiptTaxAmount.Location = new System.Drawing.Point(568, 245);
+            this.receiptTaxAmount.Name = "receiptTaxAmount";
+            this.receiptTaxAmount.Size = new System.Drawing.Size(0, 13);
+            this.receiptTaxAmount.TabIndex = 29;
+            this.receiptTaxAmount.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.AutoSize = true;
+            this.tenderedLabel.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.tenderedLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.tenderedLabel.Location = new System.Drawing.Point(377, 312);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(0, 13);
+            this.tenderedLabel.TabIndex = 30;
+            this.tenderedLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tenderedAmount
+            // 
+            this.tenderedAmount.AutoSize = true;
+            this.tenderedAmount.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.tenderedAmount.Location = new System.Drawing.Point(568, 312);
+            this.tenderedAmount.Name = "tenderedAmount";
+            this.tenderedAmount.Size = new System.Drawing.Size(0, 13);
+            this.tenderedAmount.TabIndex = 31;
+            this.tenderedAmount.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // niceDayLabel
+            // 
+            this.niceDayLabel.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.niceDayLabel.Location = new System.Drawing.Point(449, 367);
+            this.niceDayLabel.Name = "niceDayLabel";
+            this.niceDayLabel.Size = new System.Drawing.Size(100, 23);
+            this.niceDayLabel.TabIndex = 32;
+            this.niceDayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.ClientSize = new System.Drawing.Size(702, 450);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.niceDayLabel);
+            this.Controls.Add(this.tenderedAmount);
+            this.Controls.Add(this.tenderedLabel);
+            this.Controls.Add(this.receiptTaxAmount);
+            this.Controls.Add(this.receiptTaxLabel);
+            this.Controls.Add(this.itemPriceLabel);
+            this.Controls.Add(this.itemsLabel);
+            this.Controls.Add(this.receiptTitleLabel);
             this.Controls.Add(this.changeAmountLabel);
             this.Controls.Add(this.totalAmountLabel);
             this.Controls.Add(this.taxAmountLabel);
@@ -332,7 +423,14 @@
         private System.Windows.Forms.Label taxAmountLabel;
         private System.Windows.Forms.Label totalAmountLabel;
         private System.Windows.Forms.Label changeAmountLabel;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label receiptTitleLabel;
+        private System.Windows.Forms.Label itemsLabel;
+        private System.Windows.Forms.Label itemPriceLabel;
+        private System.Windows.Forms.Label receiptTaxLabel;
+        private System.Windows.Forms.Label receiptTaxAmount;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.Label tenderedAmount;
+        private System.Windows.Forms.Label niceDayLabel;
     }
 }
 
--- Cash Register/Form1.cs
+++ Cash Register/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Cash_Register
 {
@@ -85,6 +86,55 @@ namespace Cash_Register
         }
 
         private void ReceiptButton_Click(object sender, EventArgs e)
+        {
+            burgerAmount = Convert.ToInt32(textBox1.Text);
+            juiceAmount = Convert.ToInt32(textBox5.Text);
+            sliderAmount = Convert.ToInt32(textBox4.Text);
+
+            receiptTitleLabel.Text = "Bill's Burger's Inc.";
+            Refresh();
+            Thread.Sleep(1000);
+
+            itemsLabel.Text = "Hamburgers";
+            itemPriceLabel.Text = $"x{burgerAmount} @ ${burgerPrice.ToString(".00")}";
+            Refresh();
+            Thread.Sleep(500);
+            itemsLabel.Text += "\nSliders";
+            itemPriceLabel.Text += $"\nx{sliderAmount} @ ${slidersPrice.ToString(".00")}";
+            Refresh();
+            Thread.Sleep(500);
+            itemsLabel.Text += "\nBurger Juice";
+            itemPriceLabel.Text += $"\nx{juiceAmount} @ ${juicePrice.ToString(".00")}";
+            Refresh();
+            Thread.Sleep(1000);
+
+            receiptTaxLabel.Text = "Subtotal";
+            receiptTaxAmount.Text = $"${total.ToString(".00")}";
+            Refresh();
+            Thread.Sleep(500);
+            receiptTaxLabel.Text += "\nTax";
+            receiptTaxAmount.Text += $"\n${taxAmount.ToString(".00")}";
+            Refresh();
+            Thread.Sleep(500);
+            receiptTaxLabel.Text += "\nTotal";
+            receiptTaxAmount.Text += $"\n${totalAmount.ToString(".00")}";
+            Refresh();
+            Thread.Sleep(1000);
+
+            tenderedLabel.Text = "Tendered";
+            tenderedAmount.Text = $"{amountTendered.ToString(".00")}";
+            Refresh();
+            Thread.Sleep(500);
+            tenderedLabel.Text += "\nChange";
+            tenderedAmount.Text += $"\n{changeAmount.ToString(".00")}";
+            Refresh();
+            Thread.Sleep(1000);
+
+            niceDayLabel.Text = "Have a Nice Day!";
+
+        }
+
+        private void TextBox4_TextChanged(object sender, EventArgs e)
         {
 
         }
